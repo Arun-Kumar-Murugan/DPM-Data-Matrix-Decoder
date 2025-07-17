b799ab0beff6d4de23c777af344fabdadceee552
Arun-Kumar-Murugan <m.arunkumar.in@gmail.com>
2025-07-17 17:17:19 +0530

    Enhance DataMatrixDecoder with image processing methods and logging improvements; add requirements for dependencies

--- decoder.py
+++ decoder.py
@@ -3,6 +3,9 @@ import os
 import logging
 from logging import getLogger, DEBUG, StreamHandler
 import sys
+import cv2
+import numpy as np
+from pylibdmtx import pylibdmtx
 
 class DataMatrixDecoder:
     def __init__(self, image_path: str):
@@ -10,6 +13,9 @@ class DataMatrixDecoder:
         self.compatible_image_formats = ['.png', '.jpg', '.jpeg']
         self.logger = self._setup_logger()
         self.images = self._get_images()
+        self.crop_size = (400, 740, 650, 1000)  # (top, bottom, left, right)
+        self.enable_cv_show = False
+        self.morphology_kernel_size = (3, 3)
 
     def _get_images(self):
         image_files = []
@@ -17,7 +23,8 @@ class DataMatrixDecoder:
             image_files.extend(glob.glob(os.path.join(self.image_path, ext)))
 
         if not image_files:
-            self.logger.warning("No images found in the specified folder.")
+            self.logger.warning(f"No images found in the specified folder path: {self.image_path}.")
+            self.logger.warning("Please check the folder path and ensure it contains images in supported formats.")
             return None
 
         images = {}
@@ -40,3 +47,37 @@ class DataMatrixDecoder:
         logger.addHandler(stream_handler)
         logger.debug("Decoder started")
         return logger
+    
+    def _crop_image(self, image: np.ndarray):
+        top, bottom, left, right = self.crop_size
+        return image[top:bottom, left:right]
+    
+    def morphological_operations(self, image: np.ndarray):
+        kernel = np.ones(self.morphology_kernel_size, np.uint8)
+        morphology = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+        return morphology
+
+    def show_images(images:dict):
+        for name, img in images.items():
+            cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+            cv2.setWindowProperty(name, cv2.WND_PROP_TOPMOST, 1)
+            cv2.imshow(name, img)
+        cv2.waitKey(5000)
+        cv2.destroyAllWindows()
+
+    def _get_datamatrix_data(self, image_path):
+        input_image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+        if input_image is None:
+            self.logger.error("Image not found!")
+            exit()
+
+        gray = cv2.cvtColor(self._crop_image(input_image), cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        morph = self.morphological_operations(thresh)
+        
+        decoded_objects = pylibdmtx.decode(morph)
+        if not decoded_objects:
+            self.logger.warning(f"No DataMatrix found in {image_path}.")
+            return None
+        
+        return decoded_objects[0].data.decode("utf-8")
