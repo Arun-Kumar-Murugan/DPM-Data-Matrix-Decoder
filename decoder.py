bd3f5da4ccef43bfed21f651ae10ccdfa125637b
Arun-Kumar-Murugan <m.arunkumar.in@gmail.com>
2025-07-18 12:25:19 +0530

    Refactor DataMatrixDecoder initialization and enhance image processing methods; add error handling and logging improvements

--- decoder.py
+++ decoder.py
@@ -8,32 +8,46 @@ import numpy as np
 from pylibdmtx import pylibdmtx
 
 class DataMatrixDecoder:
-    def __init__(self, image_path: str):
-        self.image_path = image_path
+    def __init__(self, machine_type: str, enable_cv_show: bool = False, morphology_kernel_size: tuple = (3, 3),
+                 gaussian_blur_kernel_size: tuple = (7, 7), gaussian_blur_sigma: int = 5,
+                 threshold_value: int = 0, threshold_max_value: int = 255,
+                 cv_show_wait_time: int = 5000):
+
         self.compatible_image_formats = ['.png', '.jpg', '.jpeg']
         self.logger = self._setup_logger()
-        self.images = self._get_images()
-        self.crop_size = (400, 740, 650, 1000)  # (top, bottom, left, right)
-        self.enable_cv_show = False
-        self.morphology_kernel_size = (3, 3)
+        self.enable_cv_show = enable_cv_show
+        self.morphology_kernel_size = morphology_kernel_size
+        self.crop_size = {
+                        "machine_1": {
+                                        "top": 200,
+                                        "bottom": 800,
+                                        "left": 300,
+                                        "right": 1000
+                                    },
+                        "machine_2": {
+                                        "top": 200,
+                                        "bottom": 800,
+                                        "left": 300,
+                                        "right": 1000
+                                    },
+                        "machine_3": {
+                                        "top": 200,
+                                        "bottom": 1500,
+                                        "left": 100,
+                                        "right": 800
+                                    }
+                        }
+        self.machine_type = machine_type.lower()
 
-    def _get_images(self):
-        image_files = []
-        for ext in self.compatible_image_formats:
-            image_files.extend(glob.glob(os.path.join(self.image_path, ext)))
+        self.gaussian_blur_kernel_size = gaussian_blur_kernel_size
+        self.gaussian_blur_sigma = gaussian_blur_sigma
 
-        if not image_files:
-            self.logger.warning(f"No images found in the specified folder path: {self.image_path}.")
-            self.logger.warning("Please check the folder path and ensure it contains images in supported formats.")
-            return None
+        self.threshold_value = threshold_value
+        self.threshold_max_value = threshold_max_value
 
-        images = {}
-        for img_path in image_files:
-            img_name = os.path.basename(img_path)
-            images[img_name] = img_path
-
-        self.logger.info(f"Found {len(images)} images.")
-        return images
+        self.cv_show_wait_time = cv_show_wait_time
+        if self.machine_type not in self.crop_size:
+            raise ValueError(f"Unsupported machine type: {self.machine_type}. Supported types are: {list(self.crop_size.keys())}.")
 
     def _setup_logger(self):
         logger = getLogger("DecoderLogger")
@@ -48,36 +62,84 @@ class DataMatrixDecoder:
         logger.debug("Decoder started")
         return logger
     
-    def _crop_image(self, image: np.ndarray):
-        top, bottom, left, right = self.crop_size
-        return image[top:bottom, left:right]
-    
-    def morphological_operations(self, image: np.ndarray):
+    def _morphological_operations(self, image: np.ndarray):
         kernel = np.ones(self.morphology_kernel_size, np.uint8)
         morphology = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
         return morphology
 
-    def show_images(images:dict):
+    def _show_images(self, images: dict):
         for name, img in images.items():
             cv2.namedWindow(name, cv2.WINDOW_NORMAL)
             cv2.setWindowProperty(name, cv2.WND_PROP_TOPMOST, 1)
             cv2.imshow(name, img)
-        cv2.waitKey(5000)
+
+        cv2.waitKey(self.cv_show_wait_time)
         cv2.destroyAllWindows()
 
-    def _get_datamatrix_data(self, image_path):
+    def _focus_on_datamatrix(self, image: np.ndarray):
+        resized = cv2.resize(image, (1224, 1024))
+        crop = self.crop_size[self.machine_type]
+        return resized[crop["top"]:crop["bottom"], 
+                            crop["left"]:crop["right"]]
+
+    def _preprocess_image(self, image_path: str):
         input_image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+
         if input_image is None:
-            self.logger.error("Image not found!")
+            self.logger.error(f"Image not found in the specified path: {image_path}."
+                              " Check the file path and ensure the image exists.")
             exit()
 
-        gray = cv2.cvtColor(self._crop_image(input_image), cv2.COLOR_BGR2GRAY)
-        _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-        morph = self.morphological_operations(thresh)
+        focus_image = self._focus_on_datamatrix(input_image)
+        gauss_blur = cv2.GaussianBlur(focus_image, 
+                        self.gaussian_blur_kernel_size, self.gaussian_blur_sigma)
+
+        gray = cv2.cvtColor(gauss_blur, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, self.threshold_value, 
+                        self.threshold_max_value, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+        morph = self._morphological_operations(thresh)
         
-        decoded_objects = pylibdmtx.decode(morph)
+        if self.enable_cv_show:
+            image_show = {
+                "Original": input_image,
+                "Focused": focus_image,
+                "Grayscale ": gray,
+                "Thresholded": thresh,
+                "Morphology": morph
+            }
+            self._show_images(image_show)
+        
+        return morph
+
+    def _get_datamatrix_data(self, image_path: str, image_name: str):
+        preprocessed_image = self._preprocess_image(image_path)
+
+        if preprocessed_image is None:
+            self.logger.error(f"Failed to preprocess image: {image_name}.")
+            return
+        
+        decoded_objects = pylibdmtx.decode(preprocessed_image)
         if not decoded_objects:
-            self.logger.warning(f"No DataMatrix found in {image_path}.")
-            return None
-        
+            self.logger.warning(f"DataMatrix out of focus or not found in {image_name}.")
+            return
+
         return decoded_objects[0].data.decode("utf-8")
+    
+    def _fetch_images(self, image_path: str):
+        image_files = []
+        for ext in self.compatible_image_formats:
+            image_files.extend(glob.glob(os.path.join(image_path, ext)))
+
+        if not image_files:
+            self.logger.warning(f"No images found in the specified folder path: {image_path}."
+                                " Please check the folder path and ensure it contains images in supported formats.")
+            return None
+
+        images = {}
+        for img_path in image_files:
+            img_name = os.path.basename(img_path)
+            images[img_name] = img_path
+
+        self.logger.info(f"Found {len(images)} images.")
+        return images
