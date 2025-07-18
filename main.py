a38829b2314fa458d3c2c4cd415e7a4ee7b44a8d
Arun-Kumar-Murugan <m.arunkumar.in@gmail.com>
2025-07-18 14:45:47 +0530

    Load environment variables for image path and machine name in main.py; update DataMatrixDecoder initialization accordingly

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from decoder import DataMatrixDecoder
+from dotenv import load_dotenv
+import os
 
 def main():
-    decoder = DataMatrixDecoder(machine_name="machine_1")
-    decoder.decode_images(image_path="dpm_images/machine_1")
+    load_dotenv()
+    image_path = os.getenv("IMAGE_PATH")
+    machine_name = os.getenv("MACHINE_NAME")
+
+    decoder = DataMatrixDecoder(machine_name)
+    decoder.decode_images(image_path)
 
 if __name__ == "__main__":
     main()
